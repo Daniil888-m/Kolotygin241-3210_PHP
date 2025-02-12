ad0e0e73cd735bcb9bc35e67e12af2febe888b0b
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-02-12 16:44:35 +0300

    laba 2

--- Project/index.php
+++ Project/index.php
@@ -1,24 +1,28 @@
-<?php 
+<?php
 
-	class Cat {
-		private $name;
-		public $color;
-		public $weight;
+class Cat
+{
+	private $name;
+	public $color;
+	public $weight;
 
-		function __construct(string $name, string $color, int $age) {
-			$this->name = $name;
-			$this->age = $age;
-			$this->color = $color;
-		}
-
-		function setName(string $name) {
-			$this->name = $name; 
-		}
-
-		function getName() {
-			return $this->name;
-		}
+	function __construct(string $name, string $color, int $weight)
+	{
+		$this->name = $name;
+		$this->weight = $weight;
+		$this->color = $color;
 	}
 
-	$cat1 = new Cat('murka','green', 4);
-	var_dump($cat1);
+	function setName(string $name)
+	{
+		$this->name = $name;
+	}
+
+	function getName()
+	{
+		return $this->name;
+	}
+}
+
+$cat1 = new Cat('murka', 'green', 4);
+var_dump($cat1);
---
+++ lab2/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>2 Лаба: Текущее время</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="container">
+				<img src="top_sh.png" alt="Image">
+				<h1>2 лаба: Текущее время</h1>
+			</div>
+
+
+		</div>
+	</header>
+
+	<main>
+		<div class="container">
+			<section>
+				<h2>Текущее время и дата:</h2>
+				<p><?php
+					date_default_timezone_set('Europe/Moscow');
+					echo date("d F Y  H:i")
+					?></p>
+			</section>
+
+		</div>
+	</main>
+
+	<footer>
+		<div class="container">
+			<p>Задание для самостоятельной работы</p>
+		</div>
+	</footer>
+</body>
+
+</html>
