5c0194c917450a25c1f687fc37368abc6fdf5431
Daniil888-m <daniil.kolotygin80@gmail.com>
2025-03-13 21:42:16 +0300

    header footer without rooting

--- Project/www/index.php
+++ Project/www/index.php
@@ -11,12 +11,20 @@ spl_autoload_register(function (string $className) {
 		throw new Exception("Файл для класса {$className} не найден: {$file}");
 	}
 });
+$route = $_GET['route'] ?? '';
+
+$pattern = "~^hello/(.*)$~";
+preg_match($pattern, $route, $matches);
+
+var_dump($matches);
+echo '<br>';
 
 $controller = new src\Controllers\MainController;
-if (isset($_GET['name']) && !empty($_GET['name'])) {
-	$controller->sayHello($_GET['name']);
+
+if (!empty($matches)) {
+	$controller->sayHello($matches[1]);
 } else $controller->main();
-// var_dump($_GET['route']);
+var_dump($_GET['route']);
 $user = new src\Models\Users\User('Ivan');
 $article = new src\Models\Articles\Article('title', 'text', $user);
 
